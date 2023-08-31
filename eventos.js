9b6fea010e0b0fe0b14b01f23b4d7bd9f682e41d
LautaroRodriguez1 <lautarorodriguez182000@hotmail.com>
2023-08-31 18:08:24 -0300

    Event listener para el div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+const midiv = document.getElementById("eldiv")
+
+midiv.addEventListener("click", () =>{
+    alert("Hola! Soy el div")
+
+})
+
+})
